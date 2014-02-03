4512907f3125c505587510016d40011763856d60
Hans-Joachim Piepereit <hajo@csphere.eu>
2014-02-04 00:54:25 +0100

    detect hhvm

--- csphere/core/debug/toolbar.php
+++ csphere/core/debug/toolbar.php
@@ -141,8 +141,15 @@ class Toolbar
         // Details first to not overwrite something
         $data = $this->_formatDetails($logs);
 
-        // Constant PHP_RELEASE_VERSION adds patch release number
+        // Add PHP version and engine
         $data['php_full'] = phpversion();
+        $data['engine']   = 'PHP';
+        $sapi             = strtolower(php_sapi_name());
+
+        if ($sapi == 'srv') {
+
+            $data['engine'] = 'HHVM';
+        }
 
         // Move parsetime and memory usage to data array
         $data['memory'] = \csphere\core\files\File::size($stats['memory']);
