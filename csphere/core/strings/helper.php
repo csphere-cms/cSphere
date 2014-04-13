9f4c5b49b2b0e461823b23fcc7c643d942f5e248
Eike Broda <tom@csphere.eu>
2014-04-13 14:37:28 +0200

    String helper a bit better, code style

--- csphere/core/strings/helper.php
+++ csphere/core/strings/helper.php
@@ -31,71 +31,97 @@ class Helper
 
     /**
      * Validates an email address: tests if a string represents a valid email.
+     *
      * @param string $string the input string
+     *
      * @return bool
      **/
-    public static function isEmail($string) {
-        return (filter_var($string, FILTER_VALIDATE_EMAIL)) ? true : false;
+    public static function isEmail($string)
+    {
+        return !(filter_var($string, FILTER_VALIDATE_EMAIL) === false);
     }
 
     /**
      * Validates an IP4 address: tests if a string represents a valid IP.
+     *
      * @param string $string the input string
+     *
      * @return bool
      **/
-    public static function isIp4($string) {
-        return (filter_var($string, FILTER_VALIDATE_IP,FILTER_FLAG_IPV4)) ? true : false;
+    public static function isIp4($string)
+    {
+        return !(filter_var($string, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)
+            === false);
     }
 
     /**
      * Validates an IP6 address: tests if a string represents a valid IP.
+     *
      * @param string $string the input string
+     *
      * @return bool
      **/
-    public static function isIp6($string) {
-        return (filter_var($string, FILTER_VALIDATE_IP,FILTER_FLAG_IPV6)) ? true : false;
+    public static function isIp6($string)
+    {
+        return !(filter_var($string, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)
+            === false);
     }
     
     /**
      * Validates an URL address: tests if a string represents a valid URL.
+     *
      * @param string $string the input string
+     *
      * @return bool
      **/
-    public static function isUrl($string) {
-        return (filter_var($string, FILTER_VALIDATE_URL)) ? true : false;
+    public static function isUrl($string)
+    {
+        return !(filter_var($string, FILTER_VALIDATE_URL) === false);
     }
 
     /**
      * Validates a numeric: tests if a string represents Numeric.
+     *
      * @param string $string the input string
+     *
      * @return bool
      **/
-    public static function isNumeric($string) {
-        return (filter_var($string, FILTER_VALIDATE_INT)) ? true : false;
+
+    public static function isNumeric($string)
+    {
+           return !(filter_var($string, FILTER_VALIDATE_INT) === false);
     }
-   
     /**
-     * Validates a hexadecimal code: tests if a string is a valid hexadecimal HTML color.
+     * Validates a hexadecimal code: tests if a string is a valid hexadecimal
+     * HTML color.
+     *
      * @param string $string the input string
+     *
      * @return bool
      **/
-    public static function isHexColor($string) {
+    public static function isHexColor($string)
+    {
         return preg_match('/^#[a-f0-9]{6}$/i', $string);
     }
     
     /**
      * Validates a date: tests if a string represents a valid date.
+     *
      * @param string $string the input string
+     *
      * @return bool
      **/
-    public static function isDate($string) {
+    public static function isDate($string)
+    {
         $date = strtotime($string);
-        if(!filter_var($date, FILTER_VALIDATE_INT)) {
+        if (!filter_var($date, FILTER_VALIDATE_INT)) {
             return false;
         }
+
         $month = date('m', $date);
         $day = date('d', $date);
         $year = date('Y', $date);
+
         if (checkdate($month, $day, $year)) {
             return true;
         }
@@ -104,24 +130,30 @@ class Helper
 
     /**
      * Shortens a string.
-     * @param string $string the input string
-     * @param int $count the final length of the string
-     * @param string $placeholder the placeholder string :D     
+     *
+     * @param string $string      the input string
+     * @param int    $count       the final length of the string
+     * @param string $placeholder the placeholder string
+     *
      * @return string the short string
      **/        
-    public static function doStraightShorten($string, $count, $placeholder = '...') {
+    public static function doStraightShorten($string, $count, $placeholder = '...')
+    {
         return substr($string, 0, ($count*-1)) . $placeholder;
     }    
 
     /**
      * Shortens a string.
-     * @param string $string the input string
-     * @param int $count the final length of the string
-     * @param string $placeholder the placeholder string :D     
+     *
+     * @param string $string      the input string
+     * @param int    $count       the final length of the string
+     * @param string $placeholder the placeholder string
+     *
      * @return string the short string without breaking a word
      **/       
-    public static function doShorten($string, $count, $placeholder = ' ...') {
-        if(strlen($string) > $count) {
+    public static function doShorten($string, $count, $placeholder = ' ...')
+    {
+        if (strlen($string) > $count) {
             $string = substr($string, 0, $count);
             $string = substr($string, 0, strrpos($string, ' ')) . $placeholder;
         }
