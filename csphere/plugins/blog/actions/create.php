5ca64ffab661a577d92f4dba3220f886d33c7622
Daniel Burke <palle@csphere.eu>
2014-06-18 09:55:52 +0200

    fixed codestyle

--- csphere/plugins/blog/actions/create.php
+++ csphere/plugins/blog/actions/create.php
@@ -7,13 +7,13 @@
  *
  * @category  Plugins
  * @package   Blog
- * @author    Daniel Schalla <schalla@csphere.eu>
+ * @author    Daniel Schalla <contact@csphere.eu>
  * @copyright 2013 cSphere Team
  * @license   http://opensource.org/licenses/bsd-license Simplified BSD License
  * @link      http://www.csphere.eu
  **/
 
- // Get RAD class for this action
+// Get RAD class for this action
 $rad = new \csphere\core\rad\Create('blog');
 
 $t = new stdClass();
@@ -25,6 +25,16 @@ $record = function ($data) use ($t) {
     $t->tags = $data['blog_tags'];
     unset($data['blog_tags']);
 
+    $data['blog_date'] = time();
+
+    //@ToDo: Cancel Submit if invalid!
+	//@Schalla check this http://www.php.net/manual/en/features.file-upload.errors.php
+	if ($_FILES['blog_image']['error'] === UPLOAD_ERR_OK) {	
+		$upload = new \csphere\core\files\Upload;
+		$upload->setFilter('image');
+		$upload->uploadFile($_FILES['blog_image'], 'blog', $data['blog_title']);
+	}
+	
     return $data;
 };
 
@@ -34,8 +44,6 @@ $afterRecord = function ($data) use ($t) {
     \csphere\plugins\tags\classes\Tags::parseInputTags(
         $t->tags, 'blog', $data['blog_id']
     );
-
-    $data['blog_date']=time();
 
     return $data;
 };
